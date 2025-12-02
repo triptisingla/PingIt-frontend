59c0ea43960e3d26238bfa8445c9b7f5b7870a8b
Tripti, Tripti <Tripti.Tripti+cvent@cvent.com>
2025-12-02 20:52:38 +0530

    Stop tracking config folder

--- src/config/api.js
+++
@@ -1,4 +0,0 @@
-export const BASE_API_URL="http://localhost:8091";
-export const CLOUD_NAME = "dvekid8m4";
-export const CLOUDINARY_URL ="https://cloudinary://827546425732145:vStIxJU43wDeahVncD1BZ_YgBp8@dvekid8m4";
-export const UPLOAD_PRESET = "chatApp";
